7b5ef2eac1bea77f557eb83877417207943f04a7
vantaylo <vanessalyn.taylor@gmail.com>
2026-01-08 16:29:01 -0600

    created new project

---
+++ identicons/lib/identicons.ex
@@ -0,0 +1,18 @@
+defmodule Identicons do
+  @moduledoc """
+  Documentation for `Identicons`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Identicons.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ identicons/test/identicons_test.exs
@@ -0,0 +1,8 @@
+defmodule IdenticonsTest do
+  use ExUnit.Case
+  doctest Identicons
+
+  test "greets the world" do
+    assert Identicons.hello() == :world
+  end
+end
