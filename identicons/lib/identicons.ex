09417e53ca6a9a660da635789b783434ff76f62a
vantaylo <vanessalyn.taylor@gmail.com>
2026-01-12 18:34:49 -0600

    updated struct & chunck method, added helper methods

--- identicons/lib/identicons.ex
+++ identicons/lib/identicons.ex
@@ -7,9 +7,14 @@ defmodule Identicon do
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
-    hex
-    |> Enum.chunk(3)
-    |> Enum.map(&mirror_row/1)
+    grid =
+      hex
+      |> Enum.chunk_every(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten()
+      |> Enum.with_index()
+
+    %Identicon.Image{image | grid: grid}
   end
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
