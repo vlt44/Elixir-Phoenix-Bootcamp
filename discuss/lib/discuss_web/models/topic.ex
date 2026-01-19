16b4241dafebfe9f2095543a39707ce5050c3aa9
vantaylo <vanessalyn.taylor@gmail.com>
2026-01-19 15:18:45 -0600

    set changeset to validation data

--- discuss/lib/discuss_web/models/topic.ex
+++ discuss/lib/discuss_web/models/topic.ex
@@ -1,7 +1,14 @@
-defmodule discuss.Topic do
-  use Discuss.Web, :model
+defmodule Discuss.Topic do
+  use Ecto.Schema
+  import Ecto.Changeset
 
   schema "topics" do
     field(:title, :string)
+  end
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:title])
+    |> validate_required([:title])
   end
 end
