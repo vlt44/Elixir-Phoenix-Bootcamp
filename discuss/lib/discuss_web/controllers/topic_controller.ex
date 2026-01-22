e31bca6454e4e93266af11bf8fee3ebb6ed809e8
vantaylo <vanessalyn.taylor@gmail.com>
2026-01-21 18:22:55 -0600

    updating rendering layer due to version compatibility

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -1,4 +1,4 @@
-defmodule Discuss.TopicController do
+defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
   alias Discuss.Topic
 
@@ -6,5 +6,8 @@ defmodule Discuss.TopicController do
     changeset = Topic.changeset(%Topic{}, %{})
 
     render(conn, "new.html", changeset: changeset)
+  end
+
+  def create() do
   end
 end
--- discuss/lib/discuss_web/views/topic_view.ex
+++
@@ -1,3 +0,0 @@
-defmodule Discuss.TopicView do
-  use DiscussWeb, :view
-end
