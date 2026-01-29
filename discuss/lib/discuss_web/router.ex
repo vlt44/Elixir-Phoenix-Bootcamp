e2515c43929bcdb8ec62d70414ef32f13e7b9ba3
vantaylo <vanessalyn.taylor@gmail.com>
2026-01-29 16:24:29 -0600

    added update route

--- discuss/lib/discuss_web/router.ex
+++ discuss/lib/discuss_web/router.ex
@@ -21,6 +21,7 @@ defmodule DiscussWeb.Router do
     get("/topics/new", TopicController, :new)
     post("/topics", TopicController, :create)
     get("/topics/:id/edit", TopicController, :edit)
+    put("/topics/:id", TopicController, :update)
   end
 
   # Other scopes may use custom stacks.
