f140b862c3b1d26425647b522893db9db635bbae
vantaylo <vanessalyn.taylor@gmail.com>
2026-02-09 22:13:57 -0600

    moved GitHub OAuth config to runtime, secrets are loaded securely from the environment

--- discuss/config/config.exs
+++ discuss/config/config.exs
@@ -69,7 +69,3 @@ config :ueberauth, Ueberauth,
   providers: [
     github: {Ueberauth.Strategy.Github, []}
   ]
-
-config :ueberauth, Ueberauth.Strategy.Github.OAuth,
-  client_id: System.get_env("GITHUB_CLIENT_ID"),
-  client_secret: System.get_env("GITHUB_CLIENT_SECRET")
