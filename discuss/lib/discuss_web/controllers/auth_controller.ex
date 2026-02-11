0d67f384e64dae396325389c5c2e6de7d8c1bd76
vantaylo <vanessalyn.taylor@gmail.com>
2026-02-10 18:45:19 -0600

    created changeset for new user

--- discuss/lib/discuss_web/controllers/auth_controller.ex
+++ discuss/lib/discuss_web/controllers/auth_controller.ex
@@ -2,7 +2,15 @@ defmodule DiscussWeb.AuthController do
   use DiscussWeb, :controller
   plug(Ueberauth)
 
-  def callback(conn, _params) do
-    IO.inspect(conn.assigns[:ueberauth_auth])
+  alias DiscussWeb.User
+
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
+    user_params = %{
+      token: auth.credentials.token,
+      email: auth.info.email,
+      provider: to_string(auth.provider)
+    }
+
+    changeset = User.changeset(%User{}, user_params)
   end
 end
