3b34ba60a366c6df630f7943b58b29f32a468b57
vantaylo <vanessalyn.taylor@gmail.com>
2026-02-19 21:03:38 -0600

    updated signout route

--- discuss/lib/discuss_web/router.ex
+++ discuss/lib/discuss_web/router.ex
@@ -30,7 +30,7 @@ defmodule DiscussWeb.Router do
   scope "/auth", DiscussWeb do
     pipe_through(:browser)
 
-    get("/signout", SessionController, :signout)
+    get("/signout", AuthController, :signout)
     get("/:provider", AuthController, :request)
     get("/:provider/callback", AuthController, :callback)
   end
