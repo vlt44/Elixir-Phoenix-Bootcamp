336a8596e4d382e35f92f66aad01e55a35af32bf
vantaylo <vanessalyn.taylor@gmail.com>
2026-03-30 17:08:01 -0500

    fetch topic on join

--- discuss/lib/discuss_web/channels/comments_channel.ex
+++ discuss/lib/discuss_web/channels/comments_channel.ex
@@ -1,9 +1,12 @@
 defmodule Discuss.CommentsChannel do
   use DiscussWeb, :channel
+  alias Discuss.Topic
 
-  def join(name, _params, socket) do
-    IO.inspect(name)
-    {:ok, %{hey: "there"}, socket}
+  def join("comments:" <> topic_id, _params, socket) do
+    topic_id = String.to_integer(topic_id)
+    topic = Repo.get(Topic, topic_id)
+
+    {:ok, %{}, socket}
   end
 
   def handle_in(name, socket) do
