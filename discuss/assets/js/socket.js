1a107e540a8abca4f22362b198c740f47e9b08d2
vantaylo <vanessalyn.taylor@gmail.com>
2026-03-30 16:13:20 -0500

    refactor to determine topic id

--- discuss/assets/js/socket.js
+++ discuss/assets/js/socket.js
@@ -3,13 +3,12 @@ import { Socket } from "phoenix"
 let socket = new Socket("/socket", {})
 socket.connect()
 
-let channel = socket.channel("comments:1", {})
-channel.join()
-  .receive("ok", resp => console.log("joined", resp))
-  .receive("error", resp => console.log("failed", resp))
+const createSocket = (topicId) => {
+  let channel = socket.channel(`comments:${topicId}`, {})
+  
+  channel.join()
+    .receive("ok", resp => console.log("joined", resp))
+    .receive("error", resp => console.log("failed", resp))
+}
 
-document.querySelector('button').addEventListener("click", () => {
-  channel.push("new_comment", {body: "hi"})
-})
-
-export default socket
+window.createSocket = createSocket;
--- discuss/lib/discuss_web/channels/comments_channel.ex
+++ discuss/lib/discuss_web/channels/comments_channel.ex
@@ -2,9 +2,11 @@ defmodule Discuss.CommentsChannel do
   use DiscussWeb, :channel
 
   def join(name, _params, socket) do
-    {:ok, %{}, socket}
+    IO.inspect(name)
+    {:ok, %{hey: "there"}, socket}
   end
 
-  def handle_in() do
+  def handle_in(name, socket) do
+    {:reply, :ok, socket}
   end
 end
