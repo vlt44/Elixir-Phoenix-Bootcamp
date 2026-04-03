5f46418cfb5632ccc2c5fbcbf3db62dcd00b7f86
vantaylo <vanessalyn.taylor@gmail.com>
2026-04-03 17:38:05 -0500

    verify current socket user

--- discuss/lib/discuss_web/channels/user_socket.ex
+++ discuss/lib/discuss_web/channels/user_socket.ex
@@ -1,10 +1,14 @@
 defmodule DiscussWeb.UserSocket do
+  alias ElixirLS.LanguageServer.Plugins.Phoenix
   use Phoenix.Socket
 
   channel("comments:*", DiscussWeb.CommentsChannel)
 
-  def connect(_params, socket) do
-    {:ok, socket}
+  def connect(%{"token" => token}, socket) do
+    case Phoenix.Token.verify(socket, "key", token) do
+      {:ok, _user_id} -> {:ok, assign(socket, :user_id, _user_id)}
+      {:error, _reason} -> :error
+    end
   end
 
   def id(_socket), do: nil
