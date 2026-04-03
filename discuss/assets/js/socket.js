acedab5702eb00ea822a138fbc7c7de1abb80310
vantaylo <vanessalyn.taylor@gmail.com>
2026-04-02 20:45:24 -0500

    rendering list of comments

--- discuss/assets/js/socket.js
+++ discuss/assets/js/socket.js
@@ -7,13 +7,25 @@ const createSocket = (topicId) => {
   let channel = socket.channel(`comments:${topicId}`, {})
   
   channel.join()
-    .receive("ok", resp => console.log("joined", resp))
+    .receive("ok", resp => {
+      renderComments(resp.comments);
+    })
     .receive("error", resp => console.log("failed", resp))
 
   document.querySelector('button').addEventListener('click', () => {
     const content = document.querySelector('textarea').value;
     channel.push("comment:add", { content: content });
   });
+}
+
+function renderComments(comments) {
+  const renderedComments = comments.map(comment => {
+    return `<li class="collection-item">${comment.content}</li>`
+  });
+
+   list.innerHTML = comments
+    .map(comment => `<li class="collection-item">${comment.content}</li>`)
+    .join('');
 }
 
 window.createSocket = createSocket;
